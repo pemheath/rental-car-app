df1a78a7521855a13b1e387298f3a7d96d31467c
Kaden <kadenc@charter.net>
2023-04-14 09:58:33 -0500

    Added Lambdas, Requests, Results, Activities, Dependencies for SearchCars

--- MuddathersCarRentalServiceLambda/src/main/java/com/nashss/se/rentalcarservice/dynamodb/CarDao.java
+++ MuddathersCarRentalServiceLambda/src/main/java/com/nashss/se/rentalcarservice/dynamodb/CarDao.java
@@ -1,19 +1,39 @@
 package com.nashss.se.rentalcarservice.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.nashss.se.rentalcarservice.dynamodb.models.Car;
 
+import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CarDao {
 
+    private static final String CAR_AVAILABILITY_INDEX = "AvailabilityClassIndex";
     private final DynamoDBMapper mapper;
 
+    @Inject
     public CarDao(DynamoDBMapper mapper) {
         this.mapper = mapper;
     }
 
-    public List<Car> searchCars() {
-        return null;
+    public List<Car> searchCars(String[] criteria) {
+
+        Map<String, AttributeValue> valueMap = new HashMap<>();
+        valueMap.put(":availability", new AttributeValue().withS("AVAILABLE"));
+//        valueMap.put(":class", new AttributeValue().withS(""));
+
+        DynamoDBQueryExpression<Car> queryExpression = new DynamoDBQueryExpression<Car>()
+                .withIndexName(CAR_AVAILABILITY_INDEX)
+                .withConsistentRead(false)
+                .withKeyConditionExpression("availability = :availability")
+                .withExpressionAttributeValues(valueMap);
+
+        PaginatedQueryList<Car> carList = mapper.query(Car.class, queryExpression);
+        return carList;
     }
 }
---
+++ MuddathersCarRentalServiceLambda/src/main/java/com/nashss/se/rentalcarservice/lambda/SearchCarsLambda.java
@@ -0,0 +1,28 @@
+package com.nashss.se.rentalcarservice.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.rentalcarservice.activity.requests.SearchCarsRequest;
+import com.nashss.se.rentalcarservice.activity.results.SearchCarsResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SearchCarsLambda extends LambdaActivityRunner<SearchCarsRequest, SearchCarsResult>
+        implements RequestHandler<LambdaRequest<SearchCarsRequest>, LambdaResponse>{
+
+    private final Logger log = LogManager.getLogger();
+
+    @Override
+    public LambdaResponse handleRequest(LambdaRequest<SearchCarsRequest> input, Context context) {
+        log.info("handleRequest");
+        return super.runActivity(
+                () -> input.fromQuery(query ->
+                        SearchCarsRequest.builder()
+                                .withCriteria(query.get("q"))
+                                .build()),
+                (request, serviceComponent) ->
+                        serviceComponent.provideCarsActivity().handleRequest(request)
+        );
+    }
+
+}
